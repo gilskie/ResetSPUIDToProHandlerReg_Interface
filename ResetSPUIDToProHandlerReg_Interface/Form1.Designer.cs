442218d812bb0b8466b3b64bec2fe6f1a9bab777
begil cano <gilskie1989@gmail.com>
2019-06-04 12:53:14 +0800

    Updates on error handling etc.

---
+++ ResetSPUIDToProHandlerReg_Interface/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ResetSPUIDToProHandlerReg_Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ESPPV/EMBFRA/ESPCB - Reset SPUID/order"});
+            this.comboBox1.Location = new System.Drawing.Point(29, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(288, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(29, 39);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 1;
+            this.buttonReset.Text = "Reset Now";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(29, 81);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(173, 297);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Note: Id\'s to be separated with hard return.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 399);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ResetSPUIDToProHandlerReg_Interface/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Data.SqlClient;
+
+namespace ResetSPUIDToProHandlerReg_Interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text.Equals("ESPPV/EMBFRA/ESPCB - Reset SPUID/order")) {
+
+                if (richTextBox1.Text.Equals(""))
+                    MessageBox.Show("Empty spuid filed, please check!", "Error");
+
+                else
+                {
+                    string databaseConnection;
+                    databaseConnection = ReadConfigurationFile();
+
+                    if (databaseConnection == null)
+                        MessageBox.Show("Missing input text!");
+                    else
+                        DeleteFromDatabase(databaseConnection, richTextBox1.Text);
+                }
+            }
+            else
+                MessageBox.Show("No selection!");
+        }
+
+        private static void DeleteFromDatabase(string dbConnection, string values) {
+            
+            values = Regex.Replace(values, "\n", "','");
+            values = values.ToString().TrimEnd('\r', '\n');
+
+            int NormalOrdersCount = 0, articlesCount = 0, OrderStatusCount = 0;
+            string foundQuerriesMessage = "";
+
+            var appSettings = ConfigurationManager.AppSettings;
+
+            //SqlDataReader reader = null;
+            SqlConnection connection = new SqlConnection(dbConnection);
+            connection.Open();
+
+            try
+            {
+                if(connection.State == ConnectionState.Open && connection != null)
+                {
+                    SqlCommand cmd = new SqlCommand(@"delete from NormalOrders where OrgOrderID in('" + values + "')", connection);
+                    NormalOrdersCount = cmd.ExecuteNonQuery();
+
+                    cmd = new SqlCommand(@"delete from articles where orgorderid in('" + values + "')", connection);
+                    articlesCount = cmd.ExecuteNonQuery();
+
+                    cmd = new SqlCommand(@"delete from OrderStatus where OrderID in('" + values + "')", connection);
+                    OrderStatusCount = cmd.ExecuteNonQuery();
+
+                    if(NormalOrdersCount >= 1)
+                        foundQuerriesMessage = "NormalOrders,";
+                    if(articlesCount >= 1)
+                        foundQuerriesMessage = foundQuerriesMessage + ", Articles";
+                    if(OrderStatusCount >= 1)
+                        foundQuerriesMessage = foundQuerriesMessage + " and OrderStatus";
+
+                    MessageBox.Show("Successfully deleted the following tables: "+ foundQuerriesMessage.ToString());
+                }
+            }
+
+            catch (Exception E)
+            {
+                MessageBox.Show("Error:" + E.Message);
+            }
+
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+
+        private static string ReadConfigurationFile() {
+            try
+            {
+                var appsettings = ConfigurationManager.AppSettings;
+                string connectionString = appsettings["ConnectionString"].ToString();
+                return connectionString;
+            }
+            catch(ConfigurationErrorsException e) {
+                MessageBox.Show("Error: " + e.Message, "Error Message Box");
+            }
+            return null;
+        }
+    }
+}
